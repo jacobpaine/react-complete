59548553083dc41b73c10a20dd91f036fa36962f
Jacob Paine <jpaine@wcapra.com>
2019-09-24 10:27:32 -0500

    Fix minor typo

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import Person from "./Person/person";
 
 class App extends Component {
   state = {
-    persons: [{ name: "Jacob", age: "36" }, { name: "Arthur Dent", age: "42" }]
+    persons: [{ name: "Jacob", age: "36" }, { name: "Arthur Dent", age: "42" }],
+    showPersons: false
   };
 
-  switchNameHandler = () => {
-    this.setState({
-      persons: [
-        { name: "Arthur Dent", age: "42" },
-        { name: "Jacob", age: "36" }
-      ]
-    });
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
   };
 
   render() {
@@ -22,15 +19,19 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">React app</h1>
         </header>
-        <button onClick={this.switchNameHandler}>CLick me</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        ></Person>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        ></Person>
+        <button onClick={this.togglePersonsHandler}> Switch Name </button>
+        {this.state.showPersons === true ? (
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+            ></Person>
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+            ></Person>
+          </div>
+        ) : null}
       </div>
     );
   }
